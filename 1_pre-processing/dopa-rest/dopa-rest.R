88728883d532235d22ea31529aa2223721f50c94
Johannes Schielein <johannes.schielein@posteo.net>
2021-02-25 11:18:22 +0100

    dopa rest access test

--- 1_pre-processing/dopa-rest/dopa-rest.R
+++ 1_pre-processing/dopa-rest/dopa-rest.R
@@ -1,1 +1,6 @@
 #  dopa-rest.R
+download.file("https://dopa-services.jrc.ec.europa.eu/services/d6dopa40/protected_sites/get_wdpa_level_centroid?format=csv&wdpaid=555528898",
+              destfile = "1_pre-processing/dopa-rest/test.csv")
+
+test<-read.csv("1_pre-processing/dopa-rest/test.csv",sep="|")
+View(test)
