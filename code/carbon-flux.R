e05fa2a5a0f9a7a04606615996bd329ecca0ea0e
Om Bandhari <ombhandari@dsvm-geospatial.e1jxpv4mbzsenbsgf00fvcpi2f.ax.internal.cloudapp.net>
2021-06-29 01:46:21 +0000

    update carbon flux script

--- code/carbon-flux.R
+++ code/carbon-flux.R
@@ -4,23 +4,29 @@
 # ...of the desired grid coordinates
 library("terra")
 
-# (1) get_net_flux_carbon
+# (1) get_net_carbon_flux
 # provide Latitude and Longitude as arguments for the required grid
 # returns forest carbon net flux's raster file for selected grid
 
-get_net_carbon_flux <- function(lat, lon) {
-  # get tempir
-  mytempdir<-tempdir()
-  # create url
-  url <- paste0("https://gfw-data-lake.s3.amazonaws.com/gfw_forest_carbon_net_flux/v20210209/raw/per_hectare/",lat,"_",lon,"_net_flux_Mg_CO2e_ha_biomass_soil_forest_extent_2001_19.tif")
-  # create string for temporary file
-  destfile <- paste0(mytempdir,"/carbon_flux_",lat,"_",lon,".tif")
-  # download the file and save it to tempfolder
-  download.file(url, destfile)
-  # load in temporary raster and return it (result from last line is always returned in function in raster)
-  # raster(paste0(tempdir(),"/carbon_flux_",lat,"_",lon,".tif"))
-  return(rast(paste0(mytempdir,"/carbon_flux_",lat,"_",lon,".tif")))
+get_net_carbon_flux <- function(g) {
+  
+  tryCatch(
+    {
+      
+      # create url
+      url <- paste0("https://data-api.globalforestwatch.org/dataset/gfw_forest_carbon_net_flux/v20210331/download/geotiff?grid=10/40000&tile_id=",g,"&pixel_meaning=Mg_CO2e_ha")
+      # create string for temporary file
+      destfile <- paste0(tempdir(),g,".tif")
+      # download the file and save it to tempfolder
+      download.file(url, destfile)
+      # load in temporary raster and return it (result from last line is always returned in function in raster)
+      return(rast(paste0(tempdir(),g,".tif")))
+    },
+    error = function(e) {
+      message('Error in this line!')
+    }
+  )
 }
 
-# example
-# myRaster <- get_net_carbon_flux("20S", "060W")
+# # example
+# myRaster <- get_net_carbon_flux("00N_040W")
