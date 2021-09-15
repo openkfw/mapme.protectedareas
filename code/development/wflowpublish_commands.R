438b8849a8e1e9b740441bd16ffbb060da54c07f
Johannes Schielein <johannes.schielein@posteo.net>
2021-09-15 12:39:32 +0000

    update sampling

---
+++ code/development/wflowpublish_commands.R
@@ -0,0 +1,18 @@
+wflow_publish(
+  files = c(
+    "analysis/about.Rmd",
+    "analysis/accessibility.rmd",
+    "analysis/carbon-flux.rmd",
+    "analysis/contribute.Rmd",
+    "analysis/copernicus-land-cover.rmd",
+    "analysis/dopa-rest.rmd",
+    "analysis/gmw-mangrove.rmd",
+    "analysis/index.Rmd",
+    "analysis/license.Rmd",
+    "analysis/portfolio_analysis.Rmd",
+    "analysis/README.Rmd",
+    "analysis/terrain_ruggedness_index.rmd",
+    "analysis/worldpop-population-count.rmd",
+    "analysis/wwf_teow.rmd"
+  )
+)
