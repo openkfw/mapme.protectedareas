fbbf6b22941d0350c2e2a31d73571a9ae8e1979f
Johannes Schielein <johannes.schielein@posteo.net>
2021-11-13 20:21:31 +0000

    add section for breaks and delete unnecessary stuff.

--- code/development/spatial_matching.R
+++ code/development/spatial_matching.R
@@ -1,26 +1,25 @@
 library("cem")
+library("ggplot2")
+
 # ----- get data -----
 # load individual matching frames
    # note: those contain samples AOIs from 2002-2020 for supported PAs and non-protected areas. 
 
+# test for 2015
 matching_db<-
-  read_csv("../../datalake/mapme.protectedareas/output/matching/matching_frames/matching_frame_2015.csv")
+  read_csv("../../datalake/mapme.protectedareas/output/matching/matching_frames/matching_frame_2003.csv")
 
 # ---- apply Coarse Exact Matching -----
 
-test<-matching_data_combined%>%
-  select(!starts_with("biome"))
-
 imbalance(
-  test$treatment,
-  as.data.frame(test),
-  drop = c("treatment","UID",""))
+  matching_db$treatment,
+  as.data.frame(matching_db),
+  drop = c("treatment","UID","biome_max","country"))
 
-?imbalance
 results_matching_PAs <-
   cem("treatment",
-      as.data.frame(test),
-      drop =  c("treatment","UID"),
+      as.data.frame(matching_db),
+      drop =  c("treatment","UID","biome_max","country"),
       eval.imbalance = TRUE)
 
 # check matched successes
@@ -32,16 +31,14 @@ results_matching_PAs$imbalance
 # check variable breaks
 results_matching_PAs$breaks
 
-# 
-results_matching_PAs$group.idx
+# ----- adjust breaks -----
+# 1. Travel time: travel_time_to_nearby_cities_min_5k_10k
+ggplot(data = matching_db)+
+  geom_histogram(aes(travel_time_to_nearby_cities_min_5k_10k))
 
+results_matching_PAs$breaks$travel_time_to_nearby_cities_min_5k_10k
+# suggeste breaks 0-120 min, 120-300 min, 300 - 1440, > 1440
 
-
-# treatment data & variables that should be dropped (cutoffs are generated automatically according to Mr. Melvin)
-
-# outputs: matrix mat (output with matches for control and treatment)
-# imbalance table after. 
-# eventually "relaxed" to create several matches
 
 
 
