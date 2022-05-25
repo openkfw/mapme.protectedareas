fed0401844c1b1c200210874765992160ee1598f
Om Prakash Bhandari <56826085+Ohm-Np@users.noreply.github.com>
2022-05-26 00:18:11 +0200

    Update 02_hurricanes.R

--- code/processing-scripts/02_hurricanes.R
+++ code/processing-scripts/02_hurricanes.R
@@ -1,55 +1,83 @@
 ## Processing hurricane data for spatial impact evaluation
 # Author: Johannes Schielein
-# Last Edit: 2022-05-23
+# Last Edit: 2022-05-26
 
 # call libraries
 library(terra)
-library(sf)
-library(mapview)
+library(pbmcapply)
 
 # load input data
-# AOIs
+# AOIs - these AOIs contain polyid and CRS
 aoi <-
-  st_read(
-    "/datadrive/datalake/mapme.protectedareas/processing/fishnet/honeycomb_5_sqkm_subset.gpkg"
-  )
-
-
-# create poly_id
-aoi$poly_id<-
-  1:nrow(aoi)
-
+  vect("../test/honeycomb_5_sqkm_subset_crs_polyid.gpkg")
 # hurricane data
   # note: the preprocessed data is not available online. It was created with an own cript 
   # Ths script is part of the repository available under "code/development/hurricanes.R"  
 hurricanes<-
   rast("../../datalake/mapme.protectedareas/processing/hurricanes/2000-2020_V01/max_likely_windspeed_H01_global_2000-2020.tif")
+# since the hurricane raster contains 21 raster layers from years 2000-2020
+# lets get raster layer for year 2000 first
+l1 <- hurricanes[[1]]
+# the raster values range from 64 to 120 - which indicates the speed and also NA values denoting no hurricane at all
+# classify the hurricane raster into two classes 0 and 1 - 0 for NAs and 1 for value range (64-120)
+m <- c(
+  NA, 0, 0,
+  62, 153, 1
+)
+# create a classification matrix
+rclmat <- matrix(m,
+                 ncol = 3,
+                 byrow = TRUE)
+# classify the raster using the matrix
+classified_raster <- classify(l1,
+                              rclmat)
 
-# calculate area of maximum windspeed above 64 knots 
+# create processing routine to get the area affected and not affected by hurricanes
+hurricane_stats <- pbmclapply(1:nrow(aoi), function(i) {
 
-# create testsubset in central america
-aoi_subset<-aoi[420000:421000,]
-mapView(aoi_subset)
+  # mask the classified raster to the polygon
+  mask <- mask(
+    classified_raster,
+    aoi[i, ]
+  )
+  # compute area raster
+  area_raster <- cellSize(mask,
+    unit = "km"
+  )
+  # create patchsizes using zonal stats
+  patchsizes <- zonal(
+    area_raster,
+    mask,
+    sum
+  )
+  # rename the class
+  class_name <- data.frame(
+    max = c(0, 1),
+    classes = c("not_affected", "affected")
+  )
+  # merge patchsizes and class name data frame
+  out <- merge(
+    x = patchsizes, y = class_name,
+    by.x = colnames(patchsizes)[1],
+    by.y = colnames(class_name)[1]
+  )
+  out <- out[, -1]
+  # add polygon id to the result
+  out$poly_id <- aoi[i, ]$poly_id
+  # pivot to long format
+  result <- tidyr::pivot_longer(out,
+                                cols = "area")
+  result
+}, mc.cores = 8)
 
-# 
-hurricanes
-# check CRS
-st_crs(aoi_subset)
+# unlist the list as data frame
+final <- dplyr::bind_rows(hurricane_stats)
 
-# reproject to match CRS
-aoi_subset <- 
-  st_transform(aoi_subset, crs = 4326)
+# NOTE:
+ # The scripts above process only for year 2000. 
+ # To process for other years (say 2001), replace the line `23` as  **l2 <- hurricanes[[2]]**
+ # similarly, for year 2000, it will be **l21 <- hurricanes[[21]]**
+ # Then, generate the classified raster for this new hurricane year layer and then proceed further.
 
-# convert to terra zonal format
-aoi_subset <- 
-  vect(aoi_subset)
-
-#
-test_extract <-
-  terra::extract(hurricanes,
-                 aoi_subset)
-
-table(test_extract[,2])
-
-
-saveRDS(data, "../../datalake/mapme.protectedareas/output/polygon/grids/500m/data/honeycomb_5sqkm_hurricanes.rds")
+# directory to save the results
+# saveRDS(data, "../../datalake/mapme.protectedareas/output/polygon/grids/500m/data/honeycomb_5sqkm_hurricanes.rds")
